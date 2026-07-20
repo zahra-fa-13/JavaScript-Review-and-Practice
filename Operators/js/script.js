c2daae4a85bce7ecadae2b0265494057a3b300a2
Zahra <falihizahra13@gmail.com>
2026-07-20 20:08:37 +0330

    practice operators

---
+++ Operators/js/script.js
@@ -0,0 +1,40 @@
+// تمرین 1
+var num1 = 20;
+var num2 = 5;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+// تمرین 2
+var score = 18;
+console.log(score);
+console.log(score++);
+console.log(++score);
+console.log(score);
+console.log(score--);
+console.log(--score);
+
+// تمرین 3
+var money = 100;
+// +20%
+money = money + (money * 20 / 100);
+console.log(money);
+
+// تمرین 4
+var age = 24;
+age = age + 5;
+console.log(age);
+
+// تمرین 5
+var price = 450000;
+var count = 4;
+var result = price * count;
+console.log(result);
+
+
+// Calculator
+var number1 = 10;
+var number2 = 5;
