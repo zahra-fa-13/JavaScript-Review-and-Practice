85cef4bba3a8cb7c766b4a77827b22b772708269
Zahra <falihizahra13@gmail.com>
2026-07-23 23:47:21 +0330

    practice Function

---
+++ Function/js/script.js
@@ -0,0 +1,26 @@
+// تمرین 1
+function start(HelloFunction) {
+    return HelloFunction;
+}
+console.log(start("Hello Function"));
+
+function start2(HelloJavaScript) {
+    console.log(HelloJavaScript);
+}
+start2("Hello JavaScript");
+
+// تمرین 2
+function test(a) {}
+alert(("Welcome Zahra"));
+
+// تمرین 3
+function test(a) {
+    console.log(a);
+}
+test("Today I learned Functions")
+
+// تمرین 4
+function show(showName, showAge , showCity) {
+    console.log(showName, showAge , showCity);
+}
+show("showName : Zahra , showAge: 24 , showCity : Hashtagrd");
