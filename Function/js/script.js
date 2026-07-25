c2ea6f83de8702db3bddb3d5d7f889a81b038d3b
Zahra <falihizahra13@gmail.com>
2026-07-25 14:58:06 +0330

    Combining JS with the DOM and using an Object inside a Function

--- Function/js/script.js
+++ Function/js/script.js
@@ -30,3 +30,36 @@ function information(showInformation) {
     console.log(showInformation);
 }
 information("Name : Zahra, Age : 24, Field : Computer Engineering")
+
+// تمرین 6
+function card(commodity1) {
+    // روش یک که از object اسم رو بخونه
+    // document.getElementById('commodity').innerHTML = commodity1 ;
+    // روش 2 که از object اسم رو بخونه 
+    // روش 2 حرفه ای تره
+    document.getElementById('commodity').innerHTML = commodity1.name;
+    // document.getElementById('commodity').innerHTML = commodity1.price;
+    // document.getElementById('commodity').innerHTML = commodity1.brand;
+}
+// card('لوسیون, کرم, ریمل, خط چشم')
+var product = {
+    name: "لوسیون",
+    price: 250000,
+    brand: "مای"
+};
+
+// برای متوجه شدن اینکه کی اسم و صدا کنیم کی مقدار 
+function hello(name) {
+    console.log(name);
+}
+hello("Zahra");
+
+function sayAge(age) {
+    console.log(age);
+}
+sayAge(24);
+
+function sayHello() {
+    console.log("Hello");
+}
+sayHello();
