25f9ab951dbf5783f7c8a4f1e09ab784721fe2d1
Zahra <falihizahra13@gmail.com>
2026-07-28 23:16:29 +0330

    do loop project

---
+++ Loop/js/script.js
@@ -0,0 +1,107 @@
+// // تمرین 1
+// // اعداد 0 تا 10
+// var number = 0;
+
+// while (number <= 10) {
+//     console.log(number);
+//     number++;
+// }
+
+// // تمرین 2 
+// // اعداد 10 تا 0
+// var number2 = 10;
+
+// while (number2) {
+//     console.log(number2);
+//     number2--;
+// }
+
+// // تمرین 3 
+// // اعداد زوج 0 تا 20
+// for (let numberEven = 0; numberEven <= 20 ; numberEven+=2) {
+//     console.log(numberEven);
+// }
+
+// // تمرین 4 
+// // اعداد فرد 0 تا 20
+// for (let numberEven = 1; numberEven <= 20 ; numberEven+=2) {
+//     console.log(numberEven);
+// }
+
+// // تمرین 5
+// // اعداد 5 تا 50 که مضرب 5 باشن
+// for (let MultipleOF5 = 5; MultipleOF5 <= 50; MultipleOF5 += 5) {
+// console.log(MultipleOF5);
+// }
+
+// // تمرین 6 
+// for (let evenOdd = 0; evenOdd <= 30; evenOdd++) {
+//     console.log(evenOdd);
+
+//     if (evenOdd % 2==0) {
+//         console.log("زوج است");
+//     } else {
+//         console.log("فرد است");
+//     }
+// }
+
+// // تمرین 7
+// // FizzBuzz 
+// for (let fizz = 1; fizz <=100; fizz++) {
+//     console.log(fizz);
+//     if (fizz % 3 ==0) {
+//         console.log("fiiiiiiiiiiizz");
+//     }
+// }
+
+// // تمرین 8 
+// // مجموع اعداد ۱ تا ۱۰ را حساب کردم
+// let total = 0;
+
+// for (let sum = 1; sum <= 10; sum++) {
+//     total = total + sum;
+//     // اگر اینجا log بگیریم همه جمع هاارو نشون میده تا به 55 برسه اگر فقط جواب نهایی رو بخوایم بیرون باید ببریمش
+//     // console.log(total);
+// }
+// console.log(total);
+
+// // تمرین 9
+// // مجموع اعداد زوج بین ۱ تا ۱۰۰ را حساب کنیم
+// var sumEven = 0;
+
+// for (let enenSum = 1; enenSum <= 100; enenSum++) {
+//     if (enenSum % 2 == 0) {
+//         sumEven = sumEven + enenSum
+//         // نوشتار کوتاه تر اما تو بالایی رو نوشتی
+//         // sumEven += enenSum;
+//     }
+// }
+// console.log(sumEven);
+
+// تمرین 10
+// میانگین نمرات و جمع نمرات با آرایه 
+var scores = [18, 20, 15, 17, 19];
+var sumScore = 0;
+// تو خط پایین <= نار و < بذار چون شمارش از 0 شروع میشه
+for (let i = 0; i < scores.length; i++) {
+    // جمعشون
+    sumScore += scores[i];
+}
+// میانگین
+var average = sumScore / scores.length;
+
+console.log(sumScore);
+console.log(average);
+
+// تمرین 11
+var colors = [
+    "red",
+    "blue",
+    "green",
+    "yellow"
+];
+for (let rang = 0; rang < colors.length; rang++) {
+    // const element = colors[index];
+    console.log(colors[rang]);
+    
+}
