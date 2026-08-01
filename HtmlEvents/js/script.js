99c644e5250c97a6654797a070282677da222a66
Zahra <falihizahra13@gmail.com>
2026-08-01 21:07:24 +0330

    handling the onclick event

---
+++ HtmlEvents/js/script.js
@@ -0,0 +1,31 @@
+// تمرین 1
+// (onclick) - تغییر متن
+function changingText() {
+    document.getElementById("btn1").innerText =
+        " من هر روز در حال پیشرفت هستم. 🚀"
+}
+
+// تمرین 2
+// (onclick) - تغییر رنگ
+// لاین پایین برای اینه که رنگ بصورت رفت و برگشتی تغییر کنه
+// این تابع برای زمانیه که یک بار رنگ عوض شود 
+function changingColorP() {
+    // getElementsByClassName یک لیست از عناصر برمی‌گرداند
+    // پسسسسسسسسس
+    // [0] رو استفاده میکنیم تا به اولین کلاس  دسترسی داشته باشیم
+    document.getElementsByClassName("p1")[0].style.color = "red";
+}
+// این تابع برای زمانیه که چندبار تغییر رنگ بخواهیم
+var colorChange = false;
+
+function changingColorP() {
+
+    if (colorChange === false) {
+        document.getElementsByClassName("p1")[0].style.color = "red";
+        colorChange = true;
+    } else {
+        document.getElementsByClassName("p1")[0].style.color = "blueviolet";
+        colorChange = false;
+    }
+
+}
