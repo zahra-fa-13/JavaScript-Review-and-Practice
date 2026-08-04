15ecea4f6af2d4436f223bfe4dfb144052bd8c79
Zahra <falihizahra13@gmail.com>
2026-08-04 23:24:56 +0330

    The clock doesn't have the numbers 13 and up right now

--- Watch/js/script.js
+++ Watch/js/script.js
@@ -4,7 +4,14 @@ function clockRuner() {
     var minute = d.getMinutes();
     var second = d.getSeconds();
 
-    var clock = hour + ":" + minute + ":" + second;
+    // شروط زیر برای اینه که 13 و 14 و.. نداشته باشیم
+    var p = 'PM';
+    if (hour > 12) {
+        hour = hour - 12;
+        p = 'PM';
+    }
+
+    var clock = hour + ":" + minute + ":" + second + " " + p;
 
     document.getElementById('myClockDisplay').textContent = clock;
 
